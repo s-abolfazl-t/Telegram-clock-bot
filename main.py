68d64d3a66d0ebb2cff214f1a4cdb3747c59c863
s-abolfazl-t <kingsat1384@gmail.com>
2025-07-18 12:05:38 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from telethon import TelegramClient, functions
-from smallfont import to_small
 import asyncio
 import datetime
 
-api_id = 18479322  # عدد api_id خودت رو اینجا بذار
-api_hash = '0a5fd8e7a6df8040dcc46b7ae8ff5576'  # رشته api_hash خودت رو اینجا بذار
+api_id = 18479322
+api_hash = '0a5fd8e7a6df8040dcc46b7ae8ff5576'
 
 client = TelegramClient('overlay_clock', api_id, api_hash)
+
+# تابع تبدیل اعداد به فونت small
+def to_small(text):
+    normal = "0123456789"
+    small = "⁰¹²³⁴⁵⁶⁷⁸⁹"
+    return "".join(small[normal.index(c)] if c in normal else c for c in text)
 
 async def main():
     await client.start()
     while True:
         now = datetime.datetime.now().strftime("%H:%M")
-        small_time = to_small(now)
+        small_time = to_small(now.replace(":", ""))
         new_name = f"ᴄʀᴀᴢʏ! {small_time}"
         await client(functions.account.UpdateProfileRequest(first_name=new_name))
         await asyncio.sleep(60)
